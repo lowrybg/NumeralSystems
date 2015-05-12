7001f5e87ef81d20765eb8dc5fea62b6e17e6fbe
lowrybg <lowrybg@gmail.com>
2015-05-12 22:17:22 +0300

    Homework

---
+++ 07.OneSystemAnyOther/Base.cs
@@ -0,0 +1,76 @@
+﻿/*
+Problem 7. One system to any other
+
+    Write a program to convert from any numeral system of given base s to any other numeral system of base d (2 ≤ s, d ≤ 16).
+*/
+using System;
+using System.Linq;
+class Base
+{
+  
+    static void Main()
+    {
+        Console.Write("Number's base s: ");
+        int baseS = int.Parse(Console.ReadLine());
+        if (baseS < 2) throw new ArgumentOutOfRangeException();
+        Console.Write("\nEnter a non-negative integer number [base {0}]: ", baseS);
+        string number = StringParse();
+        Console.Write("\nConvert to base D: ");
+        int baseD = int.Parse(Console.ReadLine());
+        if (baseD < 2 || baseD > 16) throw new ArgumentOutOfRangeException();
+        string result = ConvertFromDecimalToBaseY(ConvertToDecimal(number.ToArray(), baseS), baseD);
+        if (IsValidInput(number, result, baseS, baseD))
+        {
+            Console.Write("\nResult -> {0} [base {1}] converted to [base {2}] => {3}\n\n", number, baseS, baseD, result);
+        }
+        else
+        {
+            Console.WriteLine("\n-> You have entered an invalid number!\n");
+        }
+    }
+    static string StringParse()
+    {
+        string number = Console.ReadLine();
+        // Check for incorrect number
+        for (int i = 0; i < number.Length; i++)
+            if (number[i] < 'A' && number[i] > 'Z' && number[i] < 'a' && number[i] > 'z' && number[i] < '0' && number[i] > '9')
+                throw new ArgumentException();
+        number = MakeAllLettersLarge(number);
+        return number;
+    }
+    // aff == AFF => valid input number
+    static string MakeAllLettersLarge(string number)
+    {
+        char[] digits = number.ToArray();
+        for (int i = 0; i < digits.Length; i++)
+            digits[i] = char.ToUpper(number[i]);
+        return string.Join("", digits);
+    }
+    #region [Essential Part - Conversion]
+    // Convert number [base X] to number [base 10]
+    static int ConvertToDecimal(char[] number, int baseX)
+    {
+        int result = 0;
+        for (int i = number.Length - 1, pow = 1; i >= 0; i--, pow *= baseX)
+            result += (number[i] >= 'A') ? (number[i] - 'A' + 10) * pow : (number[i] - '0') * pow;
+        return result;
+    }
+    // Convert number [base 10] to number [base Y]
+    static string ConvertFromDecimalToBaseY(int number, int baseY)
+    {
+        string result = string.Empty;
+        while (number > 0)
+        {
+            int remainder = number % baseY;
+            result = remainder >= 10 ? (char)('A' + remainder - 10) + result : remainder + result;
+            number /= baseY;
+        }
+        return result;
+    }
+    #endregion
+    // Convert the result from BaseY to baseX and compare the new result with the old result (baseX to baseY)
+    static bool IsValidInput(string number, string result, int baseX, int baseY)
+    {
+        return string.Compare(ConvertFromDecimalToBaseY(ConvertToDecimal(result.ToArray(), baseY), baseX), number) == 0 ? true : false;
+    }
+}
---
+++ DecimalToBinary/DecimalToBinary.cs
@@ -0,0 +1,48 @@
+﻿/*Problem 1. Decimal to binary
+
+    Write a program to convert decimal numbers to their binary representation.
+
+*/
+using System;
+using System.Linq;
+using System.Text;
+static class DecimalToBinary
+{
+   
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("\n{0} to binary => {1}\n", number, ConvertToBinary(number));
+    }
+    static string ConvertToBinary(int number)
+    {
+        StringBuilder binary = new StringBuilder();
+        bool negative = number < 0;
+        if (negative) number = ~number;
+        // Convert to binary
+        while (number > 0)
+        {
+            binary.Append(number % 2);
+            number /= 2;
+        }
+        if (negative) InverseBits(binary);
+        Reverse(binary);
+        return binary.Length == 0 ? "0" : binary.ToString(); ;
+    }
+    static void InverseBits(StringBuilder binary)
+    {
+        binary.Append(new string('0', 32 - binary.Length));
+        for (int i = 0; i < binary.Length; i++)
+            binary[i] = (binary[i] == '0') ? '1' : '0';
+    }
+    static void Reverse(StringBuilder binary)
+    {
+        for (int i = 0; i < binary.Length / 2; i++)
+        {
+            char swap = binary[i];
+            binary[i] = binary[binary.Length - i - 1];
+            binary[binary.Length - i - 1] = swap;
+        }
+    }
+}
